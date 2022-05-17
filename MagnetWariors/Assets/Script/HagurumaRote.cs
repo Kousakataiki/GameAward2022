66427f1fb00082cf21aa1278c92e3a28589363cc
梅村竜生 <umemura.ryuki.2000@gmail.com>
2022-05-17 13:14:41 +0900

    プレハブ変更,W3S2

--- MagnetWariors/Assets/Script/HagurumaRote.cs
+++ MagnetWariors/Assets/Script/HagurumaRote.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class HagurumaRote : MonoBehaviour
 {
 
-    [SerializeField]private int Speed = 2;
+    [SerializeField]private float Speed = 2;
 
     // Start is called before the first frame update
     void Start()
