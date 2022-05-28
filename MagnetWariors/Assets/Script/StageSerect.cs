becf056146c932855bea3b517956093e72eb5325
DESKTOP-02RGVIG\kousa <kousaka03taiki01@gmail.com>
2022-05-28 10:17:07 +0900

    ステージ一旦完成版(要修正)、ステージセレクト、被写界深度、ゴール追加済み

--- MagnetWariors/Assets/Script/StageSerect.cs
+++ MagnetWariors/Assets/Script/StageSerect.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class StageSerect : MonoBehaviour
 {
@@ -11,7 +10,7 @@ public class StageSerect : MonoBehaviour
     //ステージ一覧
     private enum StageNum
     {
-        Wind_1,
+        Wind_1 = 0,
         Wind_2,
         Wind_3,
         Wind_4,
@@ -43,7 +42,11 @@ public class StageSerect : MonoBehaviour
         Nuclear_6,
         Nuclear_7,
         Nuclear_8,
+
+        MAX_Stage
     }
+
+    private string[] StageFileName;
 
     //ヒエラルキーでステージ番号を選択
     [SerializeField]
@@ -52,6 +55,32 @@ public class StageSerect : MonoBehaviour
     //プレイヤーとのヒットフラグ
     private bool Hitflag;
 
+
+    private enum MovePhase
+    {
+        None,
+        Move,
+        Rotate,
+        Enter,
+        End,
+    }
+    [SerializeField] MovePhase Phase;
+
+    GameObject Player;
+    Animator anim;
+
+    Vector3 StartPos;
+    Vector3 TargetPos;
+    Quaternion StartRot;
+    Quaternion TargetRot;
+
+    [SerializeField] float MoveSpeed;
+    [SerializeField] float RotateSpeed;
+    [SerializeField] float EnterSpeed;
+    [SerializeField] float DoorAnimSpeed;
+
+    float AnimCnt;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -59,244 +88,122 @@ public class StageSerect : MonoBehaviour
 
         FadeObj = GameObject.Find("Fade");
         fade = FadeObj.GetComponent<Fade>();
+
+        Player = GameObject.FindWithTag("Player");
+        anim = GetComponent<Animator>();
+
+        AnimCnt = 0f;
+
+        StageFileName = new string[(int)StageNum.MAX_Stage];
+        StageFileName[0] = "W1S1";
+        StageFileName[1] = "W1S2";
+        StageFileName[2] = "W1S3";
+        StageFileName[3] = "W1S4";
+        StageFileName[4] = "W1S5";
+        StageFileName[5] = "W1S6";
+        StageFileName[6] = "W1S7";
+        StageFileName[7] = "W1S8";
+        StageFileName[8] = "W2S1";
+        StageFileName[9] = "W2S2";
+        StageFileName[10] = "W2S3";
+        StageFileName[11] = "W2S4";
+        StageFileName[12] = "W2S5";
+        StageFileName[13] = "W2S6";
+        StageFileName[14] = "W2S7";
+        StageFileName[15] = "W2S8";
+        StageFileName[16] = "W3S1";
+        StageFileName[17] = "W3S2";
+        StageFileName[18] = "W3S3";
+        StageFileName[19] = "W3S4";
+        StageFileName[20] = "W3S5";
+        StageFileName[21] = "W3S6";
+        StageFileName[22] = "W3S7";
+        StageFileName[23] = "W3S8";
+        StageFileName[24] = "W4S1";
+        StageFileName[25] = "W4S2";
+        StageFileName[26] = "W4S3";
+        StageFileName[27] = "W4S4";
+        StageFileName[28] = "W4S5";
+        StageFileName[29] = "W4S6";
+        StageFileName[30] = "W4S7";
+        StageFileName[31] = "W4S8";
     }
 
     // Update is called once per frame
     void Update()
     {
-        //プレイヤーとヒットしていて　なおかつ　Aボタンを押しているとき
-        if (Hitflag == true && Controller.GetKeyTrigger(Controller.ControllerButton.B))
+        switch (Phase)
         {
-            int ClearStageNum = SaveData.GetClearState();
+            case MovePhase.None:
+                //プレイヤーとヒットしていて　なおかつ　Aボタンを押しているとき
+                // /////接地しているときも条件に加えたい/////
+                if (Hitflag == true && Controller.GetKeyTrigger(Controller.ControllerButton.B))
+                {
+                    int ClearStageNum = SaveData.GetClearState();
+                    //それぞれのマップへ飛ぶ
+                    if (ClearStageNum >= (int)stagenum)
+                    {
+                        StartPos = Player.transform.position;
+                        TargetPos = new Vector3(gameObject.transform.position.x, StartPos.y, StartPos.z);
+                        Player.transform.LookAt(TargetPos);
+                        Destroy(Player.GetComponent<PlayerMove>());
+                        Player.GetComponent<Rigidbody>().useGravity = false;
+                        Player.GetComponent<Collider>().enabled = false;
+                        AudioManager.instance.BGMStart("PlayerWalk");
+                        Phase = MovePhase.Move;
+                    }
+                }
+                Hitflag = false;
+                break;
 
-            //それぞれのマップへ飛ぶ
-            switch (stagenum)
-            {
-                case StageNum.Wind_1:
-                    AudioManager.instance.BGMStop("BGM_StageSerect");
-                    fade.fadeOutStart(0, 0, 0, 0, "W1S1");
-                    break;
-                case StageNum.Wind_2:
-                    if (ClearStageNum >= 1)
-                    {
-                        AudioManager.instance.BGMStop("BGM_StageSerect");
-                        fade.fadeOutStart(0, 0, 0, 0, "W1S2");
-                    }
-                    break;
-                case StageNum.Wind_3:
-                    if (ClearStageNum >= 2)
-                    {
-                        AudioManager.instance.BGMStop("BGM_StageSerect");
-                        fade.fadeOutStart(0, 0, 0, 0, "W1S3");
-                    }
-                    break;
-                case StageNum.Wind_4:
-                    if (ClearStageNum >= 3)
-                    {
-                        AudioManager.instance.BGMStop("BGM_StageSerect");
-                        fade.fadeOutStart(0, 0, 0, 0, "W1S4");
-                    }
-                    break;
-                case StageNum.Wind_5:
-                    if (ClearStageNum >= 4)
-                    {
-                        AudioManager.instance.BGMStop("BGM_StageSerect");
-                        fade.fadeOutStart(0, 0, 0, 0, "W1S5");
-                    }
-                    break;
-                case StageNum.Wind_6:
-                    if (ClearStageNum >= 5)
-                    {
-                        AudioManager.instance.BGMStop("BGM_StageSerect");
-                        fade.fadeOutStart(0, 0, 0, 0, "W1S6");
-                    }
-                    break;
-                case StageNum.Wind_7:
-                    if (ClearStageNum >= 6)
-                    {
-                        AudioManager.instance.BGMStop("BGM_StageSerect");
-                        fade.fadeOutStart(0, 0, 0, 0, "W1S7");
-                    }
-                    break;
-                case StageNum.Wind_8:
-                    if (ClearStageNum >= 7)
-                    {
-                        AudioManager.instance.BGMStop("BGM_StageSerect");
-                        fade.fadeOutStart(0, 0, 0, 0, "W1S8");
-                    }
-                    break;
-                case StageNum.Fire_1:
-                    if (ClearStageNum >= 8)
-                    {
-                        AudioManager.instance.BGMStop("BGM_StageSerect");
-                        fade.fadeOutStart(0, 0, 0, 0, "W2S1");
-                    }
-                    break;
-                case StageNum.Fire_2:
-                    if (ClearStageNum >= 9)
-                    {
-                        AudioManager.instance.BGMStop("BGM_StageSerect");
-                        fade.fadeOutStart(0, 0, 0, 0, "W2S2");
-                    }
-                    break;
-                case StageNum.Fire_3:
-                    if (ClearStageNum >= 10)
-                    {
-                        AudioManager.instance.BGMStop("BGM_StageSerect");
-                        fade.fadeOutStart(0, 0, 0, 0, "W2S3");
-                    }
-                    break;
-                case StageNum.Fire_4:
-                    if (ClearStageNum >= 11)
-                    {
-                        AudioManager.instance.BGMStop("BGM_StageSerect");
-                        fade.fadeOutStart(0, 0, 0, 0, "W2S4");
-                    }
-                    break;
-                case StageNum.Fire_5:
-                    if (ClearStageNum >= 12)
-                    {
-                        AudioManager.instance.BGMStop("BGM_StageSerect");
-                        fade.fadeOutStart(0, 0, 0, 0, "W2S5");
-                    }
-                    break;
-                case StageNum.Fire_6:
-                    if (ClearStageNum >= 13)
-                    {
-                        AudioManager.instance.BGMStop("BGM_StageSerect");
-                        fade.fadeOutStart(0, 0, 0, 0, "W2S6");
-                    }
-                    break;
-                case StageNum.Fire_7:
-                    if (ClearStageNum >= 14)
-                    {
-                        AudioManager.instance.BGMStop("BGM_StageSerect");
-                        fade.fadeOutStart(0, 0, 0, 0, "W2S7");
-                    }
-                    break;
-                case StageNum.Fire_8:
-                    if (ClearStageNum >= 15)
-                    {
-                        AudioManager.instance.BGMStop("BGM_StageSerect");
-                        fade.fadeOutStart(0, 0, 0, 0, "W2S8");
-                    }
-                    break;
-                case StageNum.Water_1:
-                    if (ClearStageNum >= 16)
-                    {
-                        AudioManager.instance.BGMStop("BGM_StageSerect");
-                        fade.fadeOutStart(0, 0, 0, 0, "W3S1");
-                    }
-                    break;
-                case StageNum.Water_2:
-                    if (ClearStageNum >= 17)
-                    {
-                        AudioManager.instance.BGMStop("BGM_StageSerect");
-                        fade.fadeOutStart(0, 0, 0, 0, "W3S2");
-                    }
-                    break;
-                case StageNum.Water_3:
-                    if (ClearStageNum >= 18)
-                    {
-                        AudioManager.instance.BGMStop("BGM_StageSerect");
-                        fade.fadeOutStart(0, 0, 0, 0, "W3S3");
-                    }
-                    break;
-                case StageNum.Water_4:
-                    if (ClearStageNum >= 19)
-                    {
-                        AudioManager.instance.BGMStop("BGM_StageSerect");
-                        fade.fadeOutStart(0, 0, 0, 0, "W3S4");
-                    }
-                    break;
-                case StageNum.Water_5:
-                    if (ClearStageNum >= 20)
-                    {
-                        AudioManager.instance.BGMStop("BGM_StageSerect");
-                        fade.fadeOutStart(0, 0, 0, 0, "W3S5");
-                    }
-                    break;
-                case StageNum.Water_6:
-                    if (ClearStageNum >= 21)
-                    {
-                        AudioManager.instance.BGMStop("BGM_StageSerect");
-                        fade.fadeOutStart(0, 0, 0, 0, "W3S6");
-                    }
-                    break;
-                case StageNum.Water_7:
-                    if (ClearStageNum >= 22)
-                    {
-                        AudioManager.instance.BGMStop("BGM_StageSerect");
-                        fade.fadeOutStart(0, 0, 0, 0, "W3S7");
-                    }
-                    break;
-                case StageNum.Water_8:
-                    if (ClearStageNum >= 23)
-                    {
-                        AudioManager.instance.BGMStop("BGM_StageSerect");
-                        fade.fadeOutStart(0, 0, 0, 0, "W3S8");
-                    }
-                    break;
-                case StageNum.Nuclear_1:
-                    if (ClearStageNum >= 24)
-                    {
-                        AudioManager.instance.BGMStop("BGM_StageSerect");
-                        fade.fadeOutStart(0, 0, 0, 0, "W4S1");
-                    }
-                    break;
-                case StageNum.Nuclear_2:
-                    if (ClearStageNum >= 25)
-                    {
-                        AudioManager.instance.BGMStop("BGM_StageSerect");
-                        fade.fadeOutStart(0, 0, 0, 0, "W4S2");
-                    }
-                    break;
-                case StageNum.Nuclear_3:
-                    if (ClearStageNum >= 26)
-                    {
-                        AudioManager.instance.BGMStop("BGM_StageSerect");
-                        fade.fadeOutStart(0, 0, 0, 0, "W4S3");
-                    }
-                    break;
-                case StageNum.Nuclear_4:
-                    if (ClearStageNum >= 27)
-                    {
-                        AudioManager.instance.BGMStop("BGM_StageSerect");
-                        fade.fadeOutStart(0, 0, 0, 0, "W4S4");
-                    }
-                    break;
-                case StageNum.Nuclear_5:
-                    if (ClearStageNum >= 28)
-                    {
-                        AudioManager.instance.BGMStop("BGM_StageSerect");
-                        fade.fadeOutStart(0, 0, 0, 0, "W4S5");
-                    }
-                    break;
-                case StageNum.Nuclear_6:
-                    if (ClearStageNum >= 29)
-                    {
-                        AudioManager.instance.BGMStop("BGM_StageSerect");
-                        fade.fadeOutStart(0, 0, 0, 0, "W4S6");
-                    }
-                    break;
-                case StageNum.Nuclear_7:
-                    if (ClearStageNum >= 30)
-                    {
-                        AudioManager.instance.BGMStop("BGM_StageSerect");
-                        fade.fadeOutStart(0, 0, 0, 0, "W4S7");
-                    }
-                    break;
-                case StageNum.Nuclear_8:
-                    if (ClearStageNum >= 31)
-                    {
-                        AudioManager.instance.BGMStop("BGM_StageSerect");
-                        fade.fadeOutStart(0, 0, 0, 0, "W4S8");
-                    }
-                    break;
-            }
+            case MovePhase.Move:
+                // ドアの前まで移動
+                Player.transform.position += Player.transform.forward * MoveSpeed * Time.deltaTime;
+                // 十分ドアの前に来たら次へ
+                if (Vector3.Distance(Player.transform.position, TargetPos) < 0.1f)
+                {
+                    Player.transform.position = TargetPos;
+                    StartRot = Player.transform.rotation;
+                    TargetRot = Quaternion.identity;
+                    AnimCnt = 0f;
+                    anim.SetFloat("Speed", DoorAnimSpeed);
+                    anim.SetTrigger("Open");
+                    Phase = MovePhase.Rotate;
+                }
+                break;
+
+            case MovePhase.Rotate:
+                // ドアを向く
+                AnimCnt = Mathf.Min(1f, AnimCnt + RotateSpeed * Time.deltaTime);
+                Player.transform.rotation = Quaternion.Lerp(StartRot, TargetRot, AnimCnt);
+                // ドアを向いたら次へ
+                if (AnimCnt >= 1.0f)
+                {
+                    StartPos = Player.transform.position;
+                    TargetPos = StartPos + new Vector3(0, 0, 1.5f);
+                    AnimCnt = 0f;
+                    Phase = MovePhase.Enter;
+                }
+                break;
+
+            case MovePhase.Enter:
+                // ドアに入る
+                AnimCnt = Mathf.Min(1f, AnimCnt + MoveSpeed * Time.deltaTime);
+                Player.transform.position = Vector3.Lerp(StartPos, TargetPos, AnimCnt);
+                // ドアに入ったらシーン遷移
+                if (AnimCnt >= 1.0f)
+                {
+                    AudioManager.instance.BGMStop("PlayerWalk");
+                    fade.fadeOutStart(0, 0, 0, 0, StageFileName[(int)stagenum]);
+                    anim.SetFloat("Speed", DoorAnimSpeed);
+                    anim.SetTrigger("Close");
+                    Phase = MovePhase.End;
+                }
+                break;
+
+            case MovePhase.End:
+                break;
         }
-
-        Hitflag = false;
     }
 
     private void OnTriggerStay(Collider other)
@@ -305,6 +212,5 @@ public class StageSerect : MonoBehaviour
         {
             Hitflag = true;
         }
-    }
-
+    }   
 }
